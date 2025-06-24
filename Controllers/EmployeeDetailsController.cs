8a2bd7248ae6574d2248906bccec0308136d8c54
Shankhosuvro-G <shankhosuvro.ghosh@outlook.com>
2025-06-24 15:52:40 +0530

    Added EmployeeDetailsController

--- Controllers/EmployeeDetailsController.cs
+++ Controllers/EmployeeDetailsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Employee_Training_B.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Employee_Training_B.Controllers
 {
@@ -7,5 +9,37 @@ namespace Employee_Training_B.Controllers
     [ApiController]
     public class EmployeeDetailsController : ControllerBase
     {
+        private readonly EmpTdsContext _context;
+        public EmployeeDetailsController(EmpTdsContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{userId}")]
+        public async Task<ActionResult> GetEmployeeDetails(int userId)
+        {
+            var prorgramNames = new List<string>();
+            var userName = await _context.Users.FindAsync(userId);
+            var certificates = await _context.Certificates.Where(U=>U.TraineeId == userId).Select(c => new
+            {
+                c.Title,c.SubmittedOn,c.Id
+            }).ToListAsync();
+            var registeredPrograms = await _context.Registrations.Where(u=>u.UserId==userId).Select(r=>r.ProgramId).ToListAsync();
+            foreach (var programID in  registeredPrograms)
+            {
+                var programName = await _context.TrainingPrograms.Where(t=>t.ProgramId==programID).Select(c=>c.Title).FirstOrDefaultAsync();
+                prorgramNames.Add(programName.ToString());
+                Console.WriteLine($"PROGRAM NAME::::::{programName}");
+            }
+            var result = new
+            {
+                fullName = userName.FullName,
+                email = userName.Email,
+                certficateList = certificates,
+                programList = prorgramNames.AsEnumerable(),
+            };
+            return Ok(result);
+            
+
+        }
     }
 }
